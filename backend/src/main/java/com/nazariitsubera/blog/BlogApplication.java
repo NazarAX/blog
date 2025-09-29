b2714791e9725f0bbb033b7fb8c93ddd16f379dc
Nazarii Tsubera <nazartsubera@gmail.com>
2025-09-28 21:36:14 -0500

    Added hibernate and flyway

--- backend/src/main/java/com/nazariitsubera/blog/BlogApplication.java
+++ backend/src/main/java/com/nazariitsubera/blog/BlogApplication.java
@@ -4,10 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PersonalWebsiteApplication {
+public class BlogApplication {
+
 
     public static void main(String[] args) {
-        SpringApplication.run(PersonalWebsiteApplication.class, args);
+        SpringApplication.run(BlogApplication.class, args);
     }
 
 }
